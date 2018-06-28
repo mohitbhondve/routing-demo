4725998d592cef79a120edd627816644e48d5d37
mohitbhondve <mohit.bhondve01@gmail.com>
2018-06-28 23:53:15 +0530

    first commit

---
+++ src/app/department-list/department-list.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-department-list',
+  templateUrl: './department-list.component.html',
+  styleUrls: ['./department-list.component.css']
+})
+export class DepartmentListComponent implements OnInit {
+ 
+
+  departments = [
+    {"id":1,"name" : "Angular"},
+    {"id":2,"name" : "Node"},
+    {"id":3,"name" : "MongoDB"},
+    {"id":4,"name" : "ExpressJs"},
+    {"id":5,"name" : "Typescript"}
+  ]
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
